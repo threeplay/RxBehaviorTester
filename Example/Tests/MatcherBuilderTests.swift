f61aaed54032d0bcca304089f73f6774a6b849a2
Eliran Ben-Ezra <eliran@gc.io>
2019-03-03 16:18:33 -0500

    asserts

--- Example/Tests/MatcherBuilderTests.swift
+++ Example/Tests/MatcherBuilderTests.swift
@@ -26,8 +26,8 @@ class MatcherBuilderTests: XCTestCase {
       dsl.match { $0.id == 0 }
     }
 
-    expect(self.stream([State(id: 0)], on: matcher)) == .correct
-    expect(self.stream([State(id: 1)], on: matcher)) == .pending
+    expect(matcher.stream([State(id: 0)])) == .correct
+    expect(matcher.stream([State(id: 1)])) == .pending
   }
 
   func test_that_it_can_create_a_sequence_of_pedicate_matchers() {
@@ -66,8 +66,36 @@ class MatcherBuilderTests: XCTestCase {
     expect(matcher.stream([State(id: 2)])) == .pending
   }
 
-  private func stream(_ events: [State], on matcher: AnyKindMatcher<State>) -> MatchDecision {
-    matcher.reset()
-    return events.map { matcher.on(event: .next($0)) }.last!
+  func test_that_it_can_create_always_assertions() {
+    let matcher = sut.build { dsl in
+      dsl.assert(.always) { $0.id == 0 }
+    }
+
+    expect(matcher.stream([State(id: 0)])) == .correct
+    expect(matcher.stream([State(id: 1)])) == .failed
+  }
+
+  func test_that_it_evaluates_assertions_all_the_time_in_parallel_with_matchers() {
+    let matcher = sut.build { dsl in
+      dsl.assert(.always) { $0.id != 0 }
+      dsl.match { $0.id == 1 }
+      dsl.match { $0.id == 2 }
+    }
+
+    expect(matcher.stream([State(id: 1), State(id: 2)])) == .correct
+    expect(matcher.stream([State(id: 1), State(id: 0), State(id: 2)])) == .failed
+    expect(matcher.stream([State(id: 1), State(id: 2), State(id: 0)])) == .correct
+  }
+
+  func test_that_assertion_start_after_the_last_match() {
+    let matcher = sut.build { dsl in
+      dsl.match { $0.id != 0 }
+      dsl.assert(.always) { $0.id != 0 }
+      dsl.match { $0.id == 1 }
+    }
+
+    expect(matcher.stream([State(id: 0)])) == .pending
+    expect(matcher.stream([State(id: 1)])) == .correct
+    expect(matcher.stream([State(id: 2), State(id: 0)])) == .failed
   }
 }
--- RxBehaviorTester/Classes/Builder/MatcherBuilder.swift
+++ RxBehaviorTester/Classes/Builder/MatcherBuilder.swift
@@ -2,50 +2,16 @@
 
 import Foundation
 
-/*
-
- struct State {
-   let p: Int
- }
-
- let matcher = build(State.self) { m in
-   m.assert(.always) { $0.p != 3 }
-
-   m.match { $0.p == 1 }
-
-   m.assert { $0.p != 4 }
-
-   m.match("should be 2") { $0.p == 2 }
-   m.match(\.p, is: 2)
-   m.match(\.p, isNot: 2)
- }
-
-
- Asserts:
-   always - This should never happen in the scope being evaluated
-     Converts into
-
-     All matcher (inside a sequence matcher):
-        -> All asserts are individual
-        -> All the other matchers are part of a sequence
-
-  between - Only evaluates the assert between matches
-     Triggers when a matcher returns correct, and stops evaluating when next matcher is correct
-
-     This is done by
-
- any {
-   asserts
-   matcher
- }
-    
-
- */
 private class BuilderContext<Element> {
+  fileprivate var assertMatchers = [AnyKindMatcher<Element>]()
   fileprivate var matchers = [AnyKindMatcher<Element>]()
 
   open func add(_ matcher: AnyKindMatcher<Element>) {
     matchers.append(matcher)
+  }
+
+  open func addAssert(_ matcher: AnyKindMatcher<Element>) {
+    assertMatchers.append(matcher)
   }
 
   open func build() -> AnyKindMatcher<Element> {
@@ -56,25 +22,35 @@ private class BuilderContext<Element> {
 class MatcherBuilder<Element> {
   private class OrderedContext: BuilderContext<Element> {
     override func build() -> AnyKindMatcher<Element> {
-      return SequentialMatcher(matchers).toAny()
+      let ordered = SequentialMatcher(matchers).toAny()
+      if !assertMatchers.isEmpty {
+        return AnyMatcher(assertMatchers + [ordered]).toAny()
+      }
+      return ordered
     }
   }
 
   private class UnorderedContext: BuilderContext<Element> {
     override func build() -> AnyKindMatcher<Element> {
+      guard assertMatchers.isEmpty else { fatalError("Assert matchers cannot be used in unordered context") }
       return AllMatcher(matchers).toAny()
     }
   }
 
   private class FirstContext: BuilderContext<Element> {
     override func build() -> AnyKindMatcher<Element> {
-      return AnyMatcher(matchers).toAny()
+      return AnyMatcher(assertMatchers + matchers).toAny()
     }
   }
 
   class BuilderDSL<Element> {
     private var contextStack = [BuilderContext<Element>]()
     private var context: BuilderContext<Element> { return contextStack.last! }
+
+    enum AssertMode {
+      case always
+      case untilNextMatch
+    }
 
     func match(_ predicate: @escaping (Element) -> Bool) {
       context.add(PredicateMatcher(predicate).toAny())
@@ -86,6 +62,15 @@ class MatcherBuilder<Element> {
 
     func first(_ block: () -> Void) {
       context.add(with(FirstContext() as! BuilderContext<Element>, block: block).build())
+    }
+
+    func assert(_ mode: AssertMode, _ predicate: @escaping (Element) -> Bool) {
+      switch mode {
+      case .always:
+        context.addAssert(PredicateMatcher(decision: .failed) { !predicate($0) }.toAny())
+      default:
+        break
+      }
     }
 
     fileprivate init(initialContext: BuilderContext<Element>) {
@@ -100,7 +85,7 @@ class MatcherBuilder<Element> {
     }
 
     fileprivate func build() -> AnyKindMatcher<Element> {
-      assert(contextStack.count == 1)
+      Swift.assert(contextStack.count == 1)
       return context.build()
     }
   }
