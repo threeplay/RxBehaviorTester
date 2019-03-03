618b6293e215b14095ae526a3a9c00a5686f9f16
Eliran Ben-Ezra <eliran@gc.io>
2019-03-02 22:04:00 -0500

    all matcher unit tests

--- Example/Tests/AllMatcherTest.swift
+++ Example/Tests/AllMatcherTest.swift
@@ -4,66 +4,53 @@
 
 import XCTest
 @testable import RxBehaviorTester
-import RxSwift
+import Nimble
 
-//class AllMatcherTests: ListEvaluatorsTestCase {
-//  var sut: AllMatcher<State>!
-//
-//  override func setupSUT() -> AnyMatcher<ListEvaluatorsTestCase.State>? {
-//    sut = AllMatcher(mocks.map { $0.toAny() })
-//    return sut.toAny()
-//  }
-//
-//  func test_that_all_evaluators_get_the_reset_signal() {
-//    _ = tester.test(withTimeout: 0.1)
-//
-//    mocks.forEach {
-//      XCTAssertEqual($0.actions, [.reset])
-//    }
-//  }
-//
-//  func test_that_it_returns_correct_if_all_are_correct() {
-//    mocks.forEach { $0.alwaysReturn(.correct) }
-//    source.onNext(makeState())
-//
-//    let decision = tester.test(withTimeout: 0.1)
-//
-//    XCTAssertEqual(decision, .correct)
-//  }
-//
-//  func test_that_it_returns_failed_if_any_evaluator_failed() {
-//    mocks[0].alwaysReturn(.pending)
-//    mocks[1].alwaysReturn(.correct)
-//    mocks[2].alwaysReturn(.failed)
-//    source.onNext(makeState())
-//
-//    let decision = tester.test(withTimeout: 0.1)
-//
-//    XCTAssertEqual(decision, .failed)
-//  }
-//
-//  func test_that_it_stops_evaluating_evaluators_that_returned_correct() {
-//    let state = makeState()
-//    mocks[0].alwaysReturn(.pending)
-//    mocks[1].alwaysReturn(.correct)
-//    mocks[2].alwaysReturn(.pending)
-//    source.onNext(state)
-//    source.onNext(state)
-//
-//    _ = tester.test(withTimeout: 0.1)
-//
-//    XCTAssertEqual(mocks[0].actions, [.reset, .next(.next(state)), .next(.next(state))])
-//    XCTAssertEqual(mocks[1].actions, [.reset, .next(.next(state))])
-//    XCTAssertEqual(mocks[2].actions, [.reset, .next(.next(state)), .next(.next(state))])
-//  }
-//
-//  func test_that_it_returns_pending_if_not_all_are_correct() {
-//    mocks[0].alwaysReturn(.correct)
-//    mocks[1].alwaysReturn(.pending)
-//    mocks[2].alwaysReturn(.correct)
-//
-//    let decision = tester.test(withTimeout: 0.1)
-//
-//    XCTAssertEqual(decision, .failed)
-//  }
-//}
+class AllMatcherTests: XCTestCase {
+  var sut: AllMatcher<Int>!
+  var mocks: [MockMatcher<Int>]!
+
+  override func setUp() {
+    mocks = [MockMatcher(), MockMatcher(), MockMatcher()]
+    sut = AllMatcher(mocks.map { $0.toAny() })
+  }
+
+  func test_that_all_matchers_get_a_reset_signal() {
+    sut.reset()
+    expect(self.mocks.map { $0.actions }) == [[.reset], [.reset], [.reset]]
+  }
+
+  func test_that_it_returns_correct_if_all_matchers_are_correct() {
+    mocks.forEach { $0.alwaysReturn(.correct) }
+    expect(self.sut.on(event: .next(0))) == .correct
+  }
+
+  func test_that_it_returns_failed_if_any_matcher_failed() {
+    mocks[0].alwaysReturn(.pending)
+    mocks[1].alwaysReturn(.correct)
+    mocks[2].alwaysReturn(.failed)
+
+    expect(self.sut.on(event: .next(0))) == .failed
+  }
+
+  func test_that_it_stops_evaluating_matches_that_returned_correct() {
+    mocks[0].alwaysReturn(.pending)
+    mocks[1].alwaysReturn(.correct)
+    mocks[2].alwaysReturn(.pending)
+
+    _ = sut.on(event: .next(0))
+    _ = sut.on(event: .next(1))
+
+    expect(self.mocks[0].actions) == [.next(.next(0)), .next(.next(1))]
+    expect(self.mocks[1].actions) == [.next(.next(0))]
+    expect(self.mocks[2].actions) == [.next(.next(0)), .next(.next(1))]
+  }
+
+  func test_that_it_returns_pending_if_not_all_are_correct() {
+    mocks[0].alwaysReturn(.correct)
+    mocks[1].alwaysReturn(.pending)
+    mocks[2].alwaysReturn(.correct)
+
+    expect(self.sut.on(event: .next(0))) == .pending
+  }
+}
